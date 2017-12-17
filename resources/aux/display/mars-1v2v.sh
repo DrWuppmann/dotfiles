640721fbf2d72c0a8cf91fd49e7537102aa2a365
Kevin Kreusch <kevin.kreusch98@gmail.com>
2017-12-17 22:53:11 +0100

    Added Mars Monitor Configuration Scripts

---
+++ resources/aux/display/mars-1v2v.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --mode 2560x1440 --pos 1440x0 --rotate left --output HDMI-0 --primary --mode 2560x1440 --pos 0x0 --rotate right --output DVI-I-1 --off --output DVI-I-0 --off --output DP-1 --off --output DP-0 --off
